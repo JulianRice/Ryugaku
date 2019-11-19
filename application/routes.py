27e9d0c232e54a580c49495c378914042184622b
Julian M. Rice <rice.m.julian@gmail.com>
2019-11-19 03:48:47 -0800

    Added profile page, updated card design, base for uploading

--- application/routes.py
+++ application/routes.py
@@ -3,14 +3,21 @@ from application import app, db
 from flask import render_template, request, json, Response, redirect, flash, url_for, session, jsonify
 from application.models import ToukouUser
 from application.forms import RegisterFormToukou, LoginFormToukou
+import os
 # from flask_restplus import Resource #Handles all API requests
 # from application.course_list import course_list
 
+#############################################################################
+# INDEX
+#############################################################################
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html", index=True)
 
+#############################################################################
+# LOGIN
+#############################################################################
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginFormToukou()
@@ -19,7 +26,7 @@ def login():
         password    = form.password.data
 
         user = ToukouUser.objects(username=username).first()
-        if user and user.password == password:
+        if user and user.password == password: #Fix this later
             flash(f"{ user.lnameJp },　おかえりなさい", "success")
             session['userId']   = user.userId
             session['username'] = user.username
@@ -29,10 +36,16 @@ def login():
             flash("Incorrect password or username!", "danger")
     return render_template("login.html", login=True, form=form)
 
+#############################################################################
+# REGISTER (Incomplete)
+#############################################################################
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     return render_template("register.html", register=True)
 
+#############################################################################
+# USERS
+#############################################################################
 @app.route("/users")
 def user():
     #ToukouUser(userId=0, fnameJp="万里", lnameJp="宮沢", fnameEn="Banri", lnameEn="Miyazawa", \
@@ -41,6 +54,9 @@ def user():
     toukouUsers = ToukouUser.objects.all()
     return render_template("users.html", users=toukouUsers)
 
+#############################################################################
+# LOGOUT
+#############################################################################
 @app.route("/logout")
 def logout():
     session['user_id'] = False
@@ -48,6 +64,26 @@ def logout():
     session.pop('fullname', None)
     return redirect(url_for('index'))
 
-@app.route("/profile")
+#############################################################################
+# PROFILE
+#############################################################################
+@app.route("/profile", methods=['GET', 'POST'])
 def profile():
-    return redirect(url_for('index'))
+    username = session.get('username')
+    user = ToukouUser.objects(username=username).first()
+    if user == None:
+        flash("User not found! Error!", "warning")
+        return redirect(url_for('index'))
+    return render_template("profile.html", profile=True, user=user)
+
+#############################################################################
+# UPLOAD (Incomplete)
+#############################################################################
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        extension = os.path.splitext(file.filename)[1]
+        f_name = str(uuid.uuid4()) + extension
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
+    return json.dumps({'filename': f_name})
